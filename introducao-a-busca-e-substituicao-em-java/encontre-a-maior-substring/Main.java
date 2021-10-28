930f6c2cf1484d2d86fc247663f25fdb08875c5c
Yuri Italo <81713250+yuri-italo@users.noreply.github.com>
2021-10-28 17:26:39 -0300

    Update Main.java
    Introdução a busca e substituição em Java

--- introducao-a-busca-e-substituicao-em-java/encontre-a-maior-substring/Main.java
+++ introducao-a-busca-e-substituicao-em-java/encontre-a-maior-substring/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
+public class Main {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        String s1, s2, min, max;
+        while ((s1 = in.readLine()) != null) {
+            s2 = in.readLine();
+            if ( s1.length() > s2.length() ) {  
+                max = s1;
+                min = s2;
+            } else {
+                max = s2;
+                min = s1;
+            }
+            int minLength = min.length();
+            int maxS = minLength;
+            boolean f = true;
+            while (maxS > 0 && f) {
+                int diff = minLength - maxS;
+                for (int i = 0; i <= diff; i++) {
+                    if (max.contains(min.substring(i, i + maxS))) {
+                        f = false;
+                        maxS++;
+                        break;
+                    }
+                }
+                maxS--;
+            }
+            System.out.println(maxS);
+        }
+        out.close();
     }
 }
